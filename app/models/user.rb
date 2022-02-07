55195667e79cb82a9b333e63b8dac904bc7b4a7b
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-02-07 20:07:09 +0200

    Add name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :movies, dependent: :delete_all
+  validates_presence_of :name
 end
