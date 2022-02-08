61542c593acd81701d3d6a9688057ddbd1b118e2
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-02-08 11:23:53 +0200

    Fix user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :movies
-  resources :users, only: [:show]
   devise_for :users
+  get 'users/:id' => 'users#show', as: :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'movies#index'
