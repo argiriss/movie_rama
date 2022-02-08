f07917a71d9c8d3eadace453d6266dacb28953ec
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-02-08 11:50:18 +0200

    Add application header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def login_helper
+    if current_user.blank?
+      (link_to "Log in", new_user_session_path) +
+      " or ".html_safe +
+      (link_to "Sign up", new_user_registration_path)
+    else
+      "Welcome back ".html_safe +
+      (link_to current_user.name, current_user) +
+      " | ".html_safe +
+      (link_to "Log out", destroy_user_session_path, method: :delete)
+    end
+  end
 end
